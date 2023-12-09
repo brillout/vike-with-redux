52c915ab888dea58774c292428d29b8b5671abed
tim <tim@timschupp.de>
2023-12-09 23:24:08 +0100

    add another page to show clientside store persistence

---
+++ pages/anotherpage/+Page.jsx
@@ -0,0 +1,23 @@
+export default Page
+
+import React from 'react'
+import { navigate } from 'vike/client/router'
+import { useSelector, useDispatch } from 'react-redux'
+
+function Page() {
+  const count = useSelector((state) => state.value)
+  const dispatch = useDispatch()
+
+  const increment = () => dispatch({ type: 'counter/incremented' })
+  const decrement = () => dispatch({ type: 'counter/decremented' })
+  const navigateToIndexPage = () => navigate('/')
+
+  return (
+    <>
+      <h1>Redux-Controlled Counter</h1>
+      Count: {count}. <button onClick={increment}>++</button> <button onClick={decrement}>--</button>
+      <h1>Redux Store persists on clienside routing</h1>
+      <button onClick={navigateToIndexPage}>Go to Index Page</button>
+    </>
+  )
+}
